4c78cefbcd1243ecd526658c54d0c2a58dc98bb5
ZachFerg <Zach.Ferguson@strawbridge.net>
2021-07-28 11:10:51 -0400

    dates added into parameter url for listOrders call

--- GetListOrders.js
+++ GetListOrders.js
@@ -1,9 +1,40 @@
 require("dotenv").config();
 var axios = require("axios");
 
+function formatDate(date) {
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
+}
+
+const today = new Date();
+const yesterday = new Date(today);
+const day_before = new Date(today);
+
+yesterday.setDate(yesterday.getDate() - 1);
+day_before.setDate(day_before.getDate() - 2);
+
+const fm_today = formatDate(today);
+const fm_yesterday = formatDate(yesterday);
+const fm_day_before = formatDate(day_before);
+
+const param_url = new URL(
+  "https://api.fotomerchanthv.com/orders?page=1&limit=100&type=all&orderDir=ASC&from=&to="
+);
+const params = { from: fm_day_before, to: fm_yesterday };
+Object.keys(params).forEach((key) =>
+  param_url.searchParams.append(key, params[key])
+);
+
 var config = {
   method: "get",
-  url: "https://api.fotomerchanthv.com/orders?page=1&limit=100&type=all&orderDir=ASC&from=2021-07-25&to=2021-07-26",
+  url: param_url.href,
   headers: {
     Authorization: process.env.FM_API_KEY,
   },
@@ -14,7 +45,7 @@ async function getListOrders() {
   try {
     let res = await axios(config);
     if (res.status == 200) {
-      console.log(res.status);
+      // console.log(res.status);
       console.log("Order Call successful...");
     }
     // Don't forget to return something
@@ -25,13 +56,17 @@ async function getListOrders() {
 }
 
 getListOrders().then((data) => {
-  console.log("Need to grab all the order ID's");
+  console.log(
+    "There are a total of " +
+      data.paging.total +
+      " orders for today, grabbing order ID's"
+  );
   const orderIDList = [];
   try {
     data.orders.forEach(function (order) {
       orderIDList.push(order.id);
     });
-    // console.log(orderIDList);
+    console.log(orderIDList);
     return orderIDList;
   } catch (err) {
     console.error(err);
