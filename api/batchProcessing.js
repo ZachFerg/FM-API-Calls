7b81b4f23eea3d2fba9b47390d5dd38290c83d55
ZachFerg <Zach.Ferguson@strawbridge.net>
2021-09-29 10:33:09 -0400

    pulling data from orders table & setting batchid

--- api/batchProcessing.js
+++ api/batchProcessing.js
@@ -12,13 +12,13 @@ TO DO
 3. ORDERS TABLE
     1. assign `batchID` to each order in that batch
         1. How do we assign the batch ID?
-            1. Query the table and 
+            1. Query the table and get the last batch ID, increment by one
     2. assign `batchSequence` to each order in that batch
 4. Fotomerchant API Call. 
     1. Once the orders table has been updated, take that array of orders and make the batch call
     2. Try to replicate the request object in the api notes
     3. Get response object back
-5. FULFILLMENT TABLE
+5. BATCHES TABLE
     1. `batchID` will be assigned to `batchNumber`
     2. `fmBatchID` will be assigned from the response object
     3. `paperSurface` needs to be set to "Glossy"
@@ -31,43 +31,89 @@ TO DO
 6. ** Keep going until there are no more batches
 */
 
-function getBatchID(connection) {
-    let sql = "SELECT batchNumber FROM fulfillment ORDER BY batchNumber DESC LIMIT 1";
-    connection.query(sql, function (error, results, fields) {
-      if (error) throw error;
-      const batchNum = Object.values(results[0])[0]
-      console.log(batchNum)
-    return results;
-    });
+function formatDate(date) {
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
+}
+
+// Step 1
+async function pullOrders() {
+  // const today = formatDate(Date.now())
+  // console.log(today)
+  const today = "2021-09-24";
+
+  try {
+    let res = await axios.get(
+      `http://localhost:5000/api/orders/getOrders/${today}`
+    );
+    if (res.status == 200) {
+        console.log(res.data.length + " Orders pulled for today");
+    }
+    return res.data;
+  } catch (err) {
+    console.error(err);
   }
+}
 
-  getBatchID(connection)
+let orders = await pullOrders();
 
+// Step 2
+/**
+ *
+ * @param {array} orders - the results being returned from the DB
+ * @param {number} threshold - the maximum sum of paperLength for a batch
+ * @returns
+ */
+function setBatches(orders, threshold) {
+  let i;
+  let totalBatchLength = 0;
+  let batchArr = [];
+  let batchID = await getBatchID();
 
-// async function findMaxBatch() {
-//     const batch = await getBatchID(connection)
-//     console.log("batch is : ", batch)
-// }
+  // loop through array and add paperLength until it exceeds threshold
+  for (i = 0; i < orders.length && totalBatchLength < threshold; i++) {
+    totalBatchLength += orders[i].paperLength;
+    // push order id into array for API call
+    batchArr.push(orders[i].orderID);
+  }
+  // return an object containing an array of orderID's for a batch,
+  // the index it reached looping through the orders array,
+  // and the sum of all the paperLength in that batch
+  return { batchArr, index: i, totalBatchLength };
+}
 
-// findMaxBatch()
+// Step 3
+async function getBatchID() {
+  try {
+    const response = await axios.get(
+      "http://localhost:5000/api/batches/batches/getBatchID"
+    );
+    let batchNum = response.data[0].batchNumber;
+    return batchNum;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// function setBatches(orders, threshold) {
-//     let i
-//     let totalBatchLength = 0
-//     let batchArr = []
+// updateOrdersTable
+function updateOrdersTable() {
+  //code
+}
 
-//     // loop through array and add paperLength until it exceeds threshold
-//     for(i = 0; i < orders.length && totalBatchLength < threshold; i++) {
-//         totalBatchLength += orders[i].paperLength
-//         // push order id into array for API call
-//         batchArr.push(orders[i].orderID)
-//     }
-//     // return an object containing an array of orderID's for a batch, 
-//     // the index it reached looping through the orders array,
-//     // and the sum of all the paperLength in that batch
-//     return { batchArr, index: i, totalBatchLength }
-// }
+// Step 4
+function sendBatchInfo() {
+  // Send response to FM api call
+  // parse through response, grab certain info
+}
 
-
-// batches = setBatches(orders, 1000)
-// console.log(batches)
+// Step 5
+function updateFulfillmentTable() {
+  //code
+}
