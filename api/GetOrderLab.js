051e4745eefae67dd789fae041548f37a99c9d97
Zach Work <Zach.Ferguson@strawbridge.net>
2022-02-23 16:40:35 -0500

    re-integreting PAT to vscode

--- api/GetOrderLab.js
+++ api/GetOrderLab.js
@@ -264,6 +264,11 @@ function findPaperLength(data) {
   return paperLength;
 }
 
+/**
+ * remove unicode from strings
+ * @param {string} text
+ * @returns
+ */
 function stripEmojis(text) {
   return text.replace(
     /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
